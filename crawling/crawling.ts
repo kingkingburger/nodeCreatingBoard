9d3319dd67f332177e7ab56de9c4f406aca791b3
minho <dnjsalsgh123@gmail.com>
2022-12-13 20:24:03 +0900

    feat: 챔피언별 상대하기 어려운 적 크롤링 하기

--- crawling/crawling.ts
+++ crawling/crawling.ts
@@ -28,7 +28,7 @@ const getHtml = async (championNumber: number) => {
 const championName = async () => {
   try {
     const resultChampions: championInfo[] = [];
-    for (let i = 1; i <= 1000; i++) {
+    for (let i = 1; i <= 5; i++) {
       const html = await getHtml(i);
       if (!html) continue;
 
@@ -36,26 +36,49 @@ const championName = async () => {
       const championName = $(
         "body > main > div.contents > section > div.summary-heading > h3"
       );
-      const championObject = {
-        id: i,
-        name: championName.text().trim(),
-      };
-      resultChampions.push(championObject);
+      const hardCampionArray: string[] = [];
+      const hardCampionRateArray: string[] = [];
+      for (let i = 2; i <= 4; i++) {
+        const hardCampion = `body > main > div.contents > div.row.pb-2.champ-content-row > section:nth-child(3) > div > div.versus-difficult > a:nth-child(${i}) > div.champ-info`;
+        hardCampionArray.push($(hardCampion).text().trim());
+        const hardCampionRate = `body > main > div.contents > div.row.pb-2.champ-content-row > section:nth-child(3) > div > div.versus-difficult > a:nth-child(${i}) > div.champ-stat`;
+        const rate: string = $(hardCampionRate)
+          .text()
+          .split("\n")
+          .join("")
+          .split(" ")
+          .join("")
+          .trim()
+          .slice(-6);
+        hardCampionRateArray.push(rate);
+      }
+      console.log("챔피언 : ", championName.text().trim(), "일때");
+
+      console.log(hardCampionArray[0], hardCampionRateArray[0]);
+      console.log(hardCampionArray[1], hardCampionRateArray[1]);
+      console.log(hardCampionArray[2], hardCampionRateArray[2]);
+      console.log("----------------------");
+
+      // const championObject = {
+      //   id: i,
+      //   name: championName.text().trim(),
+      // };
+      // resultChampions.push(championObject);
     }
     return resultChampions;
   } catch (error) {}
 };
-
+championName();
 //champion이라는 테이블에 정보 넣기
-championName().then((champ) => {
-  if (!champ) {
-    return;
-  }
-  connection.connect();
-  champ.map((c) => {
-    connection.query(
-      `insert into dev.champion set champ_num=${c.id}, name='${c.name}'`
-    );
-  });
-  connection.end();
-});
+// championName().then((champ) => {
+//   if (!champ) {
+//     return;
+//   }
+//   connection.connect();
+//   champ.map((c) => {
+//     connection.query(
+//       `insert into dev.champion set champ_num=${c.id}, name='${c.name}'`
+//     );
+//   });
+//   connection.end();
+// });
